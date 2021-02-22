7daa1dcef4651883da0bf03e8e2e802f10ac3441
Sean Tarzy <seantarzy@gmail.com>
2021-02-21 22:51:51 -0500

    no-more-price

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
 var express = require("express"),
  cors = require('cors'),
 
-    // fs = require('fs'),
-    // https = require('https'),
   app = express(),
   port = process.env.PORT || 3000,
     mongoose = require("mongoose"),
-// Item = require("./api/models/itemModel"), //created model loading here
     bodyParser = require("body-parser");
 
     mongoose.Promise = global.Promise;
-
 
     mongoose.connect("mongodb://localhost/itemdb", { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -19,46 +15,9 @@ app.use(bodyParser.json());
 
 app.use(cors());
 
-const pem = require('pem');
-
-const {key, cert} = async () => {
-	try {
-		const certdir = (await fs.readdir("/etc/letsencrypt/live"))[0];
-
-		return {
-			key: await fs.readFile(`/etc/letsencrypt/live/${certdir}/privkey.pem`),
-			cert: await fs.readFile(`/etc/letsencrypt/live/${certdir}/fullchain.pem`)
-		}
-	}catch(e) {
-		return new Promise((res, rej) => {
-			pem.createCertificate({days: 1, selfSigned: true}, function (err, keys) {
-				if (err) {
-					rej();
-				}else {
-					res({key: keys.serviceKey, cert: keys.certificate});
-				}
-			});
-		});
-	}
-};
-
-console.log("certifications: ",key, cert)
-
-// var key = fs.readFileSync(__dirname + "/selfsigned.key");
-// var cert = fs.readFileSync(__dirname + "/selfsigned.crt");
-// var options = {
-//   key: key,
-//   cert: cert,
-// };
-
-// var server = https.createServer(options, app);
-
 
 var itemRoutes = require('./api/routes/itemRoutes'); //importing route
-var bitcoinPriceRoutes = require("./api/routes/bitcoinPriceRoutes");
 
-
-bitcoinPriceRoutes(app); //register the route
 itemRoutes(app);
 
 var homeRoute = require("./api/routes/homeRoute");
